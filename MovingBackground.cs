a5f046793c4dd9f9dc0a542475a607ab8f73861c
Toshihide-Sakao <70141444+Toshihide-Sakao@users.noreply.github.com>
2021-01-05 09:53:37 +0900

    made kiai

--- MovingBackground.cs
+++ MovingBackground.cs
@@ -32,24 +32,27 @@ namespace StorybrewScripts
 
             var bitmap = GetMapsetBitmap(BackgroundPath);
             var bg = GetLayer("").CreateSprite(BackgroundPath, OsbOrigin.Centre);
-            bg.Scale(StartTime, 500.0f / bitmap.Height);
+            bg.Scale(StartTime, 550.0f / bitmap.Height);
             bg.Fade(StartTime - 500, StartTime, 0, Opacity);
             bg.Fade(EndTime, EndTime + 500, Opacity, 0);
 
             Vector2 position = new Vector2(320, 240);
             Vector2 newPosition = new Vector2(320, 240);
+            float startDegree = Random(-5, -2);
             
             for (int time = StartTime; time < EndTime; time += (int)(beatDuration * 2))
             {
-                newPosition = new Vector2(position.X + Random(-3, 3), position.Y + Random(-3, 3));
-                bg.Move(OsbEasing.None, time, time + (int)(beatDuration), position, newPosition);
-                bg.Move(OsbEasing.None, time + (int)(beatDuration), time + (int)(beatDuration * 2), newPosition, position);
+                newPosition = new Vector2(position.X + Random(-6, 6), position.Y + Random(-6, 6));
+                bg.Move(OsbEasing.Out, time, time + (int)(beatDuration), position, newPosition);
+                bg.Move(OsbEasing.Out, time + (int)(beatDuration), time + (int)(beatDuration * 2), newPosition, position);
 
-                float randomdegree = Random(-2, 2);
-                bg.Rotate(OsbEasing.None, time, time + (int)(beatDuration), MathHelper.DegreesToRadians(0), MathHelper.DegreesToRadians(randomdegree));
-                bg.Rotate(OsbEasing.None, time + (int)(beatDuration), time + (int)(beatDuration * 2), MathHelper.DegreesToRadians(randomdegree), MathHelper.DegreesToRadians(0));
+                float nextStartDegree = Random(-3, -1);
+                float endDegreee = Random(1, 3);
+                bg.Rotate(OsbEasing.Out, time, time + (int)(beatDuration), MathHelper.DegreesToRadians(startDegree), MathHelper.DegreesToRadians(endDegreee));
+                bg.Rotate(OsbEasing.Out, time + (int)(beatDuration), time + (int)(beatDuration * 2), MathHelper.DegreesToRadians(endDegreee), MathHelper.DegreesToRadians(nextStartDegree));
                 
-                position = newPosition;
+                startDegree = nextStartDegree;
+                // position = newPosition;
             }
         }
     }
--- ParticleToBeat.cs
+++ ParticleToBeat.cs
@@ -14,10 +14,47 @@ namespace StorybrewScripts
 {
     public class ParticleToBeat : StoryboardObjectGenerator
     {
+        [Configurable] public int startTime = 45001;
+        [Configurable] public int endTime = 61073;
+        [Configurable] public string ballPath = "sb/fadeBall.png";
         public override void Generate()
         {
-		    
-            
+            int particleAmount = 40;
+            double beatDuration = Beatmap.GetTimingPointAt((int)startTime).BeatDuration;
+            var layer = GetLayer("");
+            float time = startTime;
+
+            int loopCount = (int)((endTime - startTime) / beatDuration);
+            for (double l = startTime; l < endTime; l += beatDuration)
+            {
+                for (int i = 0; i < particleAmount; i++)
+                {
+                    float opacity = Random(0.3f, 0.5f);
+                    float randomX = Random(-107, 747);
+                    float randomScale = Random(0.01f, 0.04f);
+                    float yPos = 510f;
+                    var particle = layer.CreateSprite(ballPath, OsbOrigin.Centre, new Vector2(randomX, yPos));
+
+                    particle.Scale(l, randomScale);
+                    particle.Fade(l, opacity);
+                    particle.Color(l, new Color4(175, 236, 250, 1));
+
+                    float uniqueTime = (float)l;
+                    for (int j = 0; j < 5; j++)
+                    {
+
+                        float nextY = yPos - Random(100, 200);
+                        particle.MoveY(OsbEasing.OutExpo, uniqueTime, uniqueTime + beatDuration, yPos, nextY);
+                        uniqueTime += (float)beatDuration;
+                        yPos = nextY;
+
+                        if (particle.PositionAt(uniqueTime).Y <= -50.0f)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
